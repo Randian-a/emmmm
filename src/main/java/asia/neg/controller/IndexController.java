f1344625c29428f8850e320efefa19aaa47afe43
mxchen <954396325@qq.com>
2019-11-21 10:35:37 +0800

    新建项目emmmm

---
+++ src/main/java/asia/neg/controller/IndexController.java
@@ -0,0 +1,32 @@
+package asia.neg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author mxchen
+ * @create 2019-11-21
+ */
+@Controller
+//@RequestMapping("/emmmm")
+public class IndexController {
+    /**
+     * index
+     * @return
+     */
+    @RequestMapping("/index")
+    public String index () {
+        return "index/index";
+    }
+
+    /**
+     * test
+     * @return
+     */
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test () {
+        return "{msg:'test'}";
+    }
+}
---
+++ src/test/java/asia/neg/emmmm/AppTest.java
@@ -0,0 +1,13 @@
+package asia.neg.emmmm;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
