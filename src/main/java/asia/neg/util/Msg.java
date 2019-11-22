2116d27c6f174ccc57fc6ac8bf4166fb25628f15
mxchen <954396325@qq.com>
2019-11-22 17:38:38 +0800

    修改工具类Msg

--- src/main/java/asia/neg/util/Msg.java
+++ src/main/java/asia/neg/util/Msg.java
@@ -4,58 +4,65 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 通用的返回的类
- *  
+ * @author mxchen
+ * 通用的返回的工具类
  */
 public class Msg {
-	// 状态码 200-成功  400-失败
-	private int code;
+    /**
+     * 状态码 200-成功  400-失败
+     */
+    private int code;
 
-	// 提示信息
-	private String msg;
+    /**
+     * 提示信息
+     */
+    private String msg;
 
-	// 用户要返回给浏览器的数据
-	private Map<String, Object> extend = new HashMap<String, Object>();
+    /**
+     * 用户要返回给浏览器的数据
+     */
+    private Map<String, Object> extend = new HashMap<String, Object>();
 
-	public static Msg success(){
-		Msg result = new Msg();
-		result.setCode(200);
-		result.setMsg("处理成功!");
-		return result;
-	}
-	public static Msg fail(){
-		Msg result = new Msg();
-		result.setCode(400);
-		result.setMsg("处理失败!");
-		return result;
-	}
+    public static Msg success() {
+        Msg result = new Msg();
+        result.setCode(200);
+        result.setMsg("处理成功!");
+        return result;
+    }
 
-	public Msg add(String key, Object value) {
-		this.getExtend().put(key, value);
-		return this;
-	}
+    public static Msg fail() {
+        Msg result = new Msg();
+        result.setCode(400);
+        result.setMsg("处理失败!");
+        return result;
+    }
 
-	public int getCode() {
-		return code;
-	}
+    public Msg add(String key, Object value) {
+        this.getExtend().put(key, value);
+        return this;
+    }
 
-	public void setCode(int code) {
-		this.code = code;
-	}
+    public int getCode() {
+        return code;
+    }
 
-	public String getMsg() {
-		return msg;
-	}
+    public void setCode(int code) {
+        this.code = code;
+    }
 
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
+    public String getMsg() {
+        return msg;
+    }
 
-	public Map<String, Object> getExtend() {
-		return extend;
-	}
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 
-	public void setExtend(Map<String, Object> extend) {
-		this.extend = extend;
-	}
+    public Map<String, Object> getExtend() {
+        return extend;
+    }
+
+    public void setExtend(Map<String, Object> extend) {
+        this.extend = extend;
+    }
 }
