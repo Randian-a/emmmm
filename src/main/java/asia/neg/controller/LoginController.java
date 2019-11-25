8477b5f5c92cc503be669a6a683ed8a1e0813096
mxchen <954396325@qq.com>
2019-11-25 16:26:36 +0800

    简单查库登陆

--- src/main/java/asia/neg/controller/LoginController.java
+++ src/main/java/asia/neg/controller/LoginController.java
@@ -1,7 +1,16 @@
 package asia.neg.controller;
 
+import asia.neg.pojo.UserExample;
+import asia.neg.pojo.UserPojo;
+import asia.neg.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.List;
 
 /**
  * @author mxchen
@@ -10,9 +19,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("loginIn")
-    public String loginIn(String username,String password){
-        System.out.println(username+" "+password);
-        return "index/index";
+    public ModelAndView loginIn(String username, String password) {
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andUnameEqualTo(username);
+        List<UserPojo> users = userService.selectByExample(userExample);
+        ModelAndView view = new ModelAndView();
+        if(users.size()<1){
+            view.addObject("msg","用户不存在");
+            view.setViewName("login/login");
+            return view;
+        }else{
+            for ( UserPojo u : users){
+                if(u.getPassword().equals(password)||u.getPassword()==password){
+                    view.setViewName("index/index");
+                    return view;
+                }else {
+                    view.addObject("msg","账号或密码错误");
+                    view.setViewName("login/login");
+                    return view;
+                }
+            }
+            return null;
+        }
     }
 }
